853c723d6345816b5e1bdf74804024e9db47b7e7
Ishan Shetty <ishan.h.shetty@gmail.com>
2019-04-09 17:32:46 -0500

    Bug fix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
 
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
-        //test
-
+    public static void main(String[] args) {
+        // Put logic sequence here
     }
 }
